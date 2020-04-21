8554aed723c6c70f702a9571393e4acaf44de38c
travischantravis <chancheukyin4463@gmail.com>
2020-04-20 17:35:31 -0700

    added reducers

--- src/components/layout/SingedInLinks.js
+++ src/components/layout/SingedInLinks.js
@@ -5,7 +5,7 @@ const SignedInLinks = () => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/">New Projects</NavLink>
+        <NavLink to="/create">New Projects</NavLink>
       </li>
       <li>
         <NavLink to="/">Log Out</NavLink>
