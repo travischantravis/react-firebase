dcd2516137bf066a9ae2c731695ca72a83777ccf
travischantravis <chancheukyin4463@gmail.com>
2020-04-24 22:42:30 -0700

    set up sign up

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,10 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 const ProjectDetails = (props) => {
   // destructing props.project
-  const { project } = props;
+  const { project, auth } = props;
+
+  // Route guarding
+  if (!auth.uid) return <Redirect to="/signin" />;
+
   if (project) {
     return (
       <div className="container section project-details">
@@ -38,6 +43,7 @@ const mapStateToProps = (state, ownProps) => {
   const project = projects ? projects[id] : null;
   return {
     project: project,
+    auth: state.firebase.auth,
   };
 };
 
